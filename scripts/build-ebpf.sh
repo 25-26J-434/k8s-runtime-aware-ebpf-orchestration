d47abc7cd4c6785513513e553175b3b4965636b9
panchaliSam <panchalisam20030117@gmail.com>
2025-12-12 00:24:38 +0530

    Add arch aware content to sh file

--- scripts/build-ebpf.sh
+++ scripts/build-ebpf.sh
@@ -1,2 +1,13 @@
+#Chaneg this content to architecture-aware content : Will not effect to the strcture
+
 #!/bin/bash
-clang -O2 -target bpf -c ebpf/component-1-daemon/kernel_monitor.c -o kernel_monitor.o
+set -euo pipefail
+
+ARCH=$(uname -m)
+ARCH_INCLUDE="/usr/include/${ARCH}-linux-gnu"
+
+clang -O2 -target bpf \
+  -I/usr/include \
+  -I${ARCH_INCLUDE} \
+  -c ebpf/component-1-daemon/kernel_monitor.c \
+  -o kernel_monitor.o
